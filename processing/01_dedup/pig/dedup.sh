818efab43f9ce743ad096ebbbfa498f5b8e0f253
Mark Grover <grover.markgrover@gmail.com>
2014-10-12 13:24:57 -0700

    Update dedup.sh

--- processing/01_dedup/pig/dedup.sh
+++ processing/01_dedup/pig/dedup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-hadoop fs -rm -r dedupedlogs
+hadoop fs -rm -r /etl/BI/casualcyclist/clicks/deduplogs/year=2014/month=10/day=10
 
-pig -x mapreduce -param raw_log_dir='duplogs' -param deduped_log_dir='dedupedlogs' dedup.pig
+pig -x mapreduce -param raw_log_dir='/etl/BI/casualcyclist/clicks/rawlogs/year=2014/month=10/day=10' -param deduped_log_dir='/etl/BI/casualcyclist/clicks/deduplogs/year=2014/month=10/day=10' dedup.pig
