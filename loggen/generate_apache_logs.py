ccc4e07bbb616ab0391c3cf5f8ddddd357de6057
Mark Grover <mgrover@cloudera.com>
2014-10-12 15:35:08 -0700

    Updating the log generation script to be more compatible with what the processing code expects Changes have been backported from the version at https://github.com/hadooparchitecturebook/hadoop-arch-book/blob/master/ch06-clickstream/HiveSessionize/log_gen.py

--- loggen/generate_apache_logs.py
+++ loggen/generate_apache_logs.py
@@ -2,7 +2,9 @@
 import time
 import datetime
 import random
-timestr = time.strftime("%Y%m%d-%H%M%S")
+
+otime = datetime.datetime(2014, 10, 10, 01, 30, 27)
+timestr =  time.strftime("%Y%m%d-%H%M%S", otime.timetuple())
  
 f = open('combined_log_'+timestr+'.log','w')
  
@@ -11,10 +13,8 @@ referers=["-","http://www.casualcyclist.com","http://bestcyclingreviews.com/top_
 resources=["/handle-bars","/stems","/wheelsets","/forks","/seatposts","/saddles","/shifters","/Store/cart.jsp?productID="]
 useragents=["Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36","Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1","Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25","Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201","Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0","Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))"]
  
-otime = datetime.datetime(2013,10,10)
- 
 for i in xrange(0,500):
-	increment = datetime.timedelta(seconds=random.randint(30,300))
+	increment = datetime.timedelta(seconds=random.randint(30,3000))
 	otime += increment
 	uri = random.choice(resources)
 	if uri.find("Store")>0:
