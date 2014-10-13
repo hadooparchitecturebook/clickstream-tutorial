591ebdb47afe757898a742565cf1484a8b06e21d
Mark Grover <mgrover@cloudera.com>
2014-10-13 13:51:09 -0700

    Updating setup to not have any removal

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 sudo -u hdfs hadoop fs -mkdir -p /etl/BI/casualcyclist/clicks/rawlogs
 sudo -u hdfs hadoop fs -chmod 1777 /etl/BI/casualcyclist/clicks/rawlogs
 
-sudo -u hdfs hadoop fs -rm -r /etl/BI/casualcyclist/clicks/sessionizedlogs/year=2014/month=10/day=10
 sudo -u hdfs hadoop fs -mkdir -p /etl/BI/casualcyclist/clicks/sessionizedlogs/year=2014/month=10/
 sudo -u hdfs hadoop fs -chmod 1777 /etl/BI/casualcyclist/clicks/sessionizedlogs/year=2014/month=10/
 
