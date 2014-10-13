170024a2d4ed58394f931ef8e9ac234c08185e71
Mark Grover <mgrover@cloudera.com>
2014-10-12 21:49:41 -0700

    Updating run_me to not do any setup work

--- 03_processing/02_sessionization/hive/run_me.sh
+++ 03_processing/02_sessionization/hive/run_me.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -x
-
-sudo -u hdfs hadoop fs -mkdir -p /data/bikeshop/clickstream
-sudo -u hdfs hadoop fs -chmod 1777 /data/bikeshop/clickstream
-
-sudo -u hdfs hadoop fs -mkdir -p /user/$USER
-sudo -u hdfs hadoop fs -chown $USER:$USER /user/$USER
-
 hive -f 01-create-raw-log-table.hql
 hive -f 02-create-parquet-log-table.hql
 hive -f 03-populate-parquet-log-table.hql
