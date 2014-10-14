898b5108c234f0581aec795eee27ac1e331530ed
Mark Grover <mgrover@cloudera.com>
2014-10-14 14:42:48 -0400

    Some updates to the orchestration setup script

--- 04_orchestration/setup.sh
+++ 04_orchestration/setup.sh
@@ -1,6 +1,8 @@
+sudo -u oozie hadoop fs -rm -r /user/oozie/share
 sudo oozie-setup sharelib create -fs hdfs://localhost:8020 --locallib /usr/lib/oozie/oozie-sharelib-yarn.tar.gz
 hdfs dfs -rm -r /app/oozie-workflows/clickstream
-hdfs dfs -mkdir -p /app/oozie-workflows/clickstream/lib
+sudo -u hdfs hdfs dfs -mkdir -p /app/oozie-workflows/clickstream/lib
+sudo -u hdfs hadoop fs -chmod -R 777 /app/oozie-workflows/clickstream
 hdfs dfs -put processing.xml /app/oozie-workflows/clickstream
 hdfs dfs -put dedup.pig /app/oozie-workflows/clickstream
 hdfs dfs -put ../03_processing/02_sessionization/mr/target/MRSessionize-1.0-SNAPSHOT.jar /app/oozie-workflows/clickstream/lib
